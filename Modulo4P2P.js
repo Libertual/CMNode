035e67138c7cd8761f4f7ace168a6ca3b20f96b3
Libertual <peterworld@gmail.com>
2015-06-25 13:56:41 +0200

    Ejercicio Entregado

--- Modulo4P2P.js
+++ Modulo4P2P.js
@@ -1,35 +1,55 @@
 var express = require('express');
 var app = express();
+// Acceso al recuso "preguntas" 
 app.get('/preguntas', function (req, res){
-	res.send( '<html><body>'
-		+ '<form method="GET" action="respuestas1">'
-		+ '¿Quién descubrió America?<br>'
-		+ '<input type="text" name="respuesta1" /><br>'
+	res.send( '<html>'
+		+ '<title>Preguntas</title>'
+		+ '<body>'
+		+ '<h1>Preguntas</h1>'
+		+ '<form method="GET" action="respuesta">'
+		+ '¿Quién descubrió América?<br>'
+		+ '<input type="text" name="respuesta" /><br>'
+		+ '<input type="hidden" name="pregunta" value="1" /><br>'
 		+ '<input type="submit" name="Enviar" />'
 		+ '</form><br>'
-		+ '<form method="GET" action="respuestas2">'
+		+ '<form method="GET" action="respuesta">'
 		+ '¿Capital de Portugal?<br>'
-		+ '<input type="text" name="respuesta2" /><br>'
+		+ '<input type="text" name="respuesta" /><br>'
+		+ '<input type="hidden" name="pregunta" value="2" /><br>'
 		+ '<input type="submit" name="Enviar" />'
 		+ '</form>'
 		+ '</body></html>'
 		 );
 	});
-app.get('/respuestas*', function (req, res){
+// Acceso al recusro "respuestas"
+app.get('/respuesta', function (req, res){
+	var correcta = "";
+	// Evaluamos la pregunta
+	switch(req.query.pregunta){
+	case "1":
+		correcta = "Cristobal Colón";
+		break;
+	case "2": 
+		correcta = "Lisboa";
+		break;
+	default:
+		res.redirect(400, '/preguntas');
+		return;
 
-	if (req.query.respuesta1 === "Cristobal Colón")
-	{
-		msg = "Respuesta Correcta";
-	}
-	else {
-		msg = "Respuesta incorrecta, la respuesta correcta es Cristbal Colón";
-	}
+	} 
+	if (req.query.respuesta === correcta) msg = '¡Respuesta Correcta!';
+	else msg = '¡Respuesta incorrecta! La respuesta correcta es "' + correcta + '"';
 
-	res.send( '<html><body>'
+	res.send( '<html>'
+		+ '<title>Respuestas</title>'
+		+ '<body>'
 		+ msg + "<br>"
+		+ '<a href="/preguntas">Volver</a>'
 		+ '</body></html>'
 		 );
 	});
-app.get('*', function (req, res){res.send( 'Nada' );});
-app.listen(8080);
+// Si no encuentra el recurso muestra un página de advertencia
+app.get('*', function (req, res){res.send( '<html><body>Página no encontrada</body></html>' );});
+console.log ("Aplicacion iniciada en http://localhost/preguntas")
+app.listen(80);
 
